7813fe5fd06507e437c38fb562ab8e09644ec0e9
bangoura-mama <mbangoura.1507@gmail.com>
2023-10-05 13:15:24 +0000

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { LacroseComponent } from './lacrose/lacrose.component';
+import { AccComponent } from './acc/acc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcceuilComponent
+    AcceuilComponent,
+    LacroseComponent,
+    AccComponent
   ],
   imports: [
     BrowserModule,
